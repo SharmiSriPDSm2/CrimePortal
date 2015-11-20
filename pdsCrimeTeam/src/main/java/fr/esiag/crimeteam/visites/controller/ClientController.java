be77aa867e90dfb85946aaca4672f84843e308a9
Bohbot <Bohbot@10.211.55.4>
2015-11-20 10:40:18 +0100

    test commit bohbot

--- pdsCrimeTeam/src/main/java/fr/esiag/crimeteam/visites/controller/ClientController.java
+++ pdsCrimeTeam/src/main/java/fr/esiag/crimeteam/visites/controller/ClientController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.support.SessionStatus;
-
+//test bohbot
 //ceci est un tzqt JLI
 
 // test BNG
